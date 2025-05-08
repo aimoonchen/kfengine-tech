c957d96590895b62db5ca77908ed5753f23c6b44
chenmou <chenmou@corp.netease.com>
2025-05-08 19:47:49 +0800

    daily commit

---
+++ filament/backend/include/private/backend/CommandStream.h
@@ -0,0 +1,204 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TNT_FILAMENT_BACKEND_PRIVATE_COMMANDSTREAM_H
+#define TNT_FILAMENT_BACKEND_PRIVATE_COMMANDSTREAM_H
+
+#include "private/backend/CircularBuffer.h"
+//#include "private/backend/Dispatcher.h"
+#include "private/backend/Driver.h"
+
+#include <backend/BufferDescriptor.h>
+#include <backend/BufferObjectStreamDescriptor.h>
+#include <backend/CallbackHandler.h>
+#include <backend/DriverEnums.h>
+#include <backend/Handle.h>
+#include <backend/PipelineState.h>
+#include <backend/Program.h>
+#include <backend/PixelBufferDescriptor.h>
+// #include <backend/PresentCallable.h>
+// #include <backend/TargetBufferInfo.h>
+
+#include <utils/compiler.h>
+#include <utils/debug.h>
+#include <utils/ThreadUtils.h>
+#include <math/mathfwd.h>
+
+#include <cstddef>
+#include <functional>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+
+#ifndef NDEBUG
+#include <thread>
+#endif
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+
+// Set to true to print every command out on log.d. This requires RTTI and DEBUG
+#define DEBUG_COMMAND_STREAM false
+
+namespace filament::backend {
+
+class CommandStream {
+    template<typename T>
+    struct AutoExecute {
+        T closure;
+        inline explicit AutoExecute(T&& closure) : closure(std::forward<T>(closure)) {}
+        inline ~AutoExecute() { closure(); }
+    };
+
+public:
+    CommandStream(Driver& driver, CircularBuffer& buffer) noexcept;
+
+    CommandStream(CommandStream const& rhs) noexcept = delete;
+    CommandStream& operator=(CommandStream const& rhs) noexcept = delete;
+
+    CircularBuffer const& getCircularBuffer() const noexcept { return mCurrentBuffer; }
+
+	bool isWorkaroundNeeded(Workaround) { return false; }
+	FeatureLevel getFeatureLevel() { return FeatureLevel::FEATURE_LEVEL_1; }
+	math::float2 getClipSpaceParams() { return math::float2{ 1.0f, 0.0f }; }
+	uint8_t getMaxDrawBuffers() {
+        return 16;// MRT::MAX_SUPPORTED_RENDER_TARGET_COUNT;
+	}
+	size_t getMaxUniformBufferSize() { return 16384u; }
+	size_t getMaxTextureSize(SamplerType target) {
+		// NoopDriver is being actively used for other purposes.  This needs to be resolved before we
+		// can change it to 2048. b/406832484
+		return 16384u;
+	}
+	size_t getMaxArrayTextureLayers() { return 256u; }
+	void updateIndexBuffer(Handle<HwIndexBuffer> ibh, BufferDescriptor&& p, uint32_t byteOffset) {}
+	void updateBufferObject(Handle<HwBufferObject> ibh, BufferDescriptor&& p, uint32_t byteOffset) {}
+	void destroyBufferObject(Handle<HwBufferObject> boh) {}
+	void destroyTexture(Handle<HwTexture> th) {}
+	void destroyProgram(Handle<HwProgram> ph) {}
+    backend::ProgramHandle createProgram(backend::Program&& program) { return {}; }
+    backend::BufferObjectHandle createBufferObject(
+        uint32_t byteCount,
+        backend::BufferObjectBinding bindingType,
+        backend::BufferUsage usage) { return {}; }
+    void setDebugTag(backend::HandleBase::HandleId handleId, utils::CString tag) {}
+	bool isStereoSupported() { return false; }
+	bool isParallelShaderCompileSupported() { return false; }
+	void compilePrograms(CompilerPriorityQueue priority, CallbackHandler* handler, CallbackHandler::Callback callback, void* user) {}
+    void registerBufferObjectStreams(Handle<HwBufferObject> boh, BufferObjectStreamDescriptor&& streams) {}
+
+    void updateDescriptorSetBuffer(
+        backend::DescriptorSetHandle dsh,
+        backend::descriptor_binding_t binding,
+        backend::BufferObjectHandle boh,
+        uint32_t offset,
+        uint32_t size
+    ) {}
+    void updateDescriptorSetTexture(
+        backend::DescriptorSetHandle dsh,
+        backend::descriptor_binding_t binding,
+        backend::TextureHandle th,
+        SamplerParams params
+    ) {}
+	backend::DescriptorSetHandle createDescriptorSet(backend::DescriptorSetLayoutHandle dslh) { return {}; }
+    void destroyDescriptorSet(backend::DescriptorSetHandle dsh) {}
+	void bindDescriptorSet(
+		backend::DescriptorSetHandle dsh,
+		backend::descriptor_set_t set,
+		backend::DescriptorSetOffsetArray&& offsets) {}
+	void destroyVertexBuffer(Handle<HwVertexBuffer> vbh) {}
+	void destroyIndexBuffer(Handle<HwIndexBuffer> ibh) {}
+    backend::IndexBufferHandle createIndexBuffer(
+        backend::ElementType elementType,
+        uint32_t indexCount,
+        backend::BufferUsage usage) { return {}; }
+    backend::VertexBufferHandle createVertexBuffer(
+        uint32_t vertexCount,
+        backend::VertexBufferInfoHandle vbih) { return {}; }
+    void setVertexBufferObject(
+        backend::VertexBufferHandle vbh,
+        uint32_t index,
+        backend::BufferObjectHandle bufferObject) {}
+
+public:
+    // This is for debugging only. Currently, CircularBuffer can only be written from a
+    // single thread. In debug builds we assert this condition.
+    // Call this first in the render loop.
+    inline void debugThreading() noexcept {
+#ifndef NDEBUG
+        mThreadId = utils::ThreadUtils::getThreadId();
+#endif
+    }
+
+    void execute(void* buffer);
+
+    /*
+     * queueCommand() allows to queue a lambda function as a command.
+     * This is much less efficient than using the Driver* API.
+     */
+    void queueCommand(std::function<void()> command);
+
+    /*
+     * Allocates memory associated to the current CommandStreamBuffer.
+     * This memory will be automatically freed after this command buffer is processed.
+     * IMPORTANT: Destructors ARE NOT called
+     */
+    inline void* allocate(size_t size, size_t alignment = 8) noexcept;
+
+    /*
+     * Helper to allocate an array of trivially destructible objects
+     */
+    template<typename PodType,
+            typename = typename std::enable_if<std::is_trivially_destructible<PodType>::value>::type>
+    inline PodType* allocatePod(
+            size_t count = 1, size_t alignment = alignof(PodType)) noexcept;
+
+private:
+    inline void* allocateCommand(size_t size) {
+        assert_invariant(utils::ThreadUtils::isThisThread(mThreadId));
+        return mCurrentBuffer.allocate(size);
+    }
+
+    // We use a copy of Dispatcher (instead of a pointer) because this removes one dereference
+    // when executing driver commands.
+    Driver& UTILS_RESTRICT mDriver;
+    CircularBuffer& UTILS_RESTRICT mCurrentBuffer;
+    //Dispatcher mDispatcher;
+
+#ifndef NDEBUG
+    // just for debugging...
+    std::thread::id mThreadId{};
+#endif
+
+    bool mUsePerformanceCounter = false;
+};
+
+void* CommandStream::allocate(size_t size, size_t alignment) noexcept {
+    // make sure alignment is a power of two
+    assert_invariant(alignment && !(alignment & alignment-1));
+
+    return (char*)allocateCommand(size);
+}
+
+template<typename PodType, typename>
+PodType* CommandStream::allocatePod(size_t count, size_t alignment) noexcept {
+    return static_cast<PodType*>(allocate(count * sizeof(PodType), alignment));
+}
+
+} // namespace filament::backend
+
+#endif // TNT_FILAMENT_BACKEND_PRIVATE_COMMANDSTREAM_H
---
+++ filament/src/FilamentBuilder.cpp
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <filament/FilamentAPI.h>
+#include <details/Engine.h>
+#include <details/VertexBuffer.h>
+#include <details/Material.h>
+#include <backend/DriverEnums.h>
+#include <private/backend/Driver.h>
+#include <algorithm>
+
+namespace filament {
+
+void builderMakeName(utils::CString& outName, const char* name, size_t const len) noexcept {
+    if (!name) {
+        return;
+    }
+    size_t const length = std::min(len, size_t { 128u });
+    outName = utils::CString(name, length);
+}
+const Engine::Config& Engine::getConfig() const noexcept {
+	return downcast(this)->getConfig();
+}
+
+backend::FeatureLevel Engine::getActiveFeatureLevel() const noexcept {
+	return downcast(this)->getActiveFeatureLevel();
+}
+
+template<typename T, typename ... ARGS>
+T* FEngine::create(ResourceList<T>& list,
+	typename T::Builder const& builder, ARGS&& ... args) noexcept {
+	T* p = mHeapAllocator.make<T>(*this, builder, std::forward<ARGS>(args)...);
+	if (UTILS_LIKELY(p)) {
+		list.insert(p);
+	}
+	return p;
+}
+
+FBufferObject* FEngine::createBufferObject(const BufferObject::Builder& builder) noexcept {
+	return create(mBufferObjects, builder);
+}
+
+FVertexBuffer* FEngine::createVertexBuffer(const VertexBuffer::Builder& builder) noexcept {
+	return create(mVertexBuffers, builder);
+}
+
+FIndexBuffer* FEngine::createIndexBuffer(const IndexBuffer::Builder& builder) noexcept {
+	return create(mIndexBuffers, builder);
+}
+FMaterial* FEngine::createMaterial(const Material::Builder& builder,
+	std::unique_ptr<MaterialParser> materialParser) noexcept {
+	return create(mMaterials, builder, std::move(materialParser));
+}
+
+size_t backend::Driver::getElementTypeSize(backend::ElementType type) noexcept {
+	switch (type) {
+	case backend::ElementType::BYTE:     return sizeof(int8_t);
+	case backend::ElementType::BYTE2:    return sizeof(math::byte2);
+	case backend::ElementType::BYTE3:    return sizeof(math::byte3);
+	case backend::ElementType::BYTE4:    return sizeof(math::byte4);
+	case backend::ElementType::UBYTE:    return sizeof(uint8_t);
+	case backend::ElementType::UBYTE2:   return sizeof(math::ubyte2);
+	case backend::ElementType::UBYTE3:   return sizeof(math::ubyte3);
+	case backend::ElementType::UBYTE4:   return sizeof(math::ubyte4);
+	case backend::ElementType::SHORT:    return sizeof(int16_t);
+	case backend::ElementType::SHORT2:   return sizeof(math::short2);
+	case backend::ElementType::SHORT3:   return sizeof(math::short3);
+	case backend::ElementType::SHORT4:   return sizeof(math::short4);
+	case backend::ElementType::USHORT:   return sizeof(uint16_t);
+	case backend::ElementType::USHORT2:  return sizeof(math::ushort2);
+	case backend::ElementType::USHORT3:  return sizeof(math::ushort3);
+	case backend::ElementType::USHORT4:  return sizeof(math::ushort4);
+	case backend::ElementType::INT:      return sizeof(int32_t);
+	case backend::ElementType::UINT:     return sizeof(uint32_t);
+	case backend::ElementType::FLOAT:    return sizeof(float);
+	case backend::ElementType::FLOAT2:   return sizeof(math::float2);
+	case backend::ElementType::FLOAT3:   return sizeof(math::float3);
+	case backend::ElementType::FLOAT4:   return sizeof(math::float4);
+	case backend::ElementType::HALF:     return sizeof(math::half);
+	case backend::ElementType::HALF2:    return sizeof(math::half2);
+	case backend::ElementType::HALF3:    return sizeof(math::half3);
+	case backend::ElementType::HALF4:    return sizeof(math::half4);
+	}
+}
+} // namespace filament
