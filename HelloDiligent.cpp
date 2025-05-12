d0b1739bfcd19ce7f873566e2bd617f51bcd9422
aimoonchen <kungfuchen@gmail.com>
2025-05-12 22:56:08 +0800

    daily commit

--- HelloDiligent.cpp
+++ HelloDiligent.cpp
@@ -106,47 +106,77 @@ static size_t fileSize(int fd) {
  // Diligent Engine can use HLSL source on all supported platforms.
  // It will convert HLSL to GLSL in OpenGL mode, while Vulkan backend will compile it directly to SPIRV.
  
- static const char* VSSource = R"(
+static const char* VSSource =
+R"(
 cbuffer Constants
 {
     float4x4 g_WorldViewProj;
 };
 
-// Vertex shader takes two inputs: vertex position and uv coordinates.
+// Vertex shader takes two inputs: vertex position and color.
 // By convention, Diligent Engine expects vertex shader inputs to be 
 // labeled 'ATTRIBn', where n is the attribute number.
 struct VSInput
 {
-    float3 Pos : ATTRIB0;
-    float2 UV  : ATTRIB1;
+    float3 Pos   : ATTRIB0;
+    float4 Color : ATTRIB1;
 };
 
 struct PSInput 
-{ 
-    float4 Pos : SV_POSITION; 
-    float2 UV  : TEX_COORD; 
+{
+    float4 Pos   : SV_POSITION;
+    float4 Color : COLOR0; 
 };
 
 // Note that if separate shader objects are not supported (this is only the case for old GLES3.0 devices), vertex
 // shader output variable name must match exactly the name of the pixel shader input variable.
 // If the variable has structure type (like in this example), the structure declarations must also be identical.
 void main(in  VSInput VSIn,
-          out PSInput PSIn) 
+          out PSInput PSIn)
 {
-    PSIn.Pos = mul( float4(VSIn.Pos,1.0), g_WorldViewProj);
-    PSIn.UV  = VSIn.UV;
+    PSIn.Pos   = mul(float4(VSIn.Pos, 1.0), g_WorldViewProj);
+    PSIn.Color = VSIn.Color;
 }
- )";
+)";
+// 	 R"(
+// cbuffer Constants
+// {
+//     float4x4 g_WorldViewProj;
+// };
+// 
+// // Vertex shader takes two inputs: vertex position and uv coordinates.
+// // By convention, Diligent Engine expects vertex shader inputs to be 
+// // labeled 'ATTRIBn', where n is the attribute number.
+// struct VSInput
+// {
+//     float3 Pos : ATTRIB0;
+//     float2 UV  : ATTRIB1;
+// };
+// 
+// struct PSInput 
+// { 
+//     float4 Pos : SV_POSITION; 
+//     float2 UV  : TEX_COORD; 
+// };
+// 
+// // Note that if separate shader objects are not supported (this is only the case for old GLES3.0 devices), vertex
+// // shader output variable name must match exactly the name of the pixel shader input variable.
+// // If the variable has structure type (like in this example), the structure declarations must also be identical.
+// void main(in  VSInput VSIn,
+//           out PSInput PSIn) 
+// {
+//     PSIn.Pos = mul( float4(VSIn.Pos,1.0), g_WorldViewProj);
+//     PSIn.UV  = VSIn.UV;
+// }
+//  )";
  
  // Pixel shader simply outputs interpolated vertex color
- static const char* PSSource = R"(
-Texture2D    g_Texture;
-SamplerState g_Texture_sampler; // By convention, texture samplers must use the '_sampler' suffix
-
+ static const char* PSSource =
+	 R"(
 struct PSInput
 {
-    float4 Pos : SV_POSITION;
-    float2 UV  : TEX_COORD;
+    float4 Pos   : SV_POSITION;
+    float4 Color : COLOR0;
 };
 
 struct PSOutput
@@ -154,17 +184,46 @@ struct PSOutput
     float4 Color : SV_TARGET;
 };
 
+// Note that if separate shader objects are not supported (this is only the case for old GLES3.0 devices), vertex
+// shader output variable name must match exactly the name of the pixel shader input variable.
+// If the variable has structure type (like in this example), the structure declarations must also be identical.
 void main(in  PSInput  PSIn,
           out PSOutput PSOut)
 {
-    float4 Color = g_Texture.Sample(g_Texture_sampler, PSIn.UV);
+    float4 Color = PSIn.Color;
 #if CONVERT_PS_OUTPUT_TO_GAMMA
     // Use fast approximation for gamma correction.
     Color.rgb = pow(Color.rgb, float3(1.0 / 2.2, 1.0 / 2.2, 1.0 / 2.2));
 #endif
     PSOut.Color = Color;
 }
- )";
+)";
+// 	 R"(
+// Texture2D    g_Texture;
+// SamplerState g_Texture_sampler; // By convention, texture samplers must use the '_sampler' suffix
+// 
+// struct PSInput
+// {
+//     float4 Pos : SV_POSITION;
+//     float2 UV  : TEX_COORD;
+// };
+// 
+// struct PSOutput
+// {
+//     float4 Color : SV_TARGET;
+// };
+// 
+// void main(in  PSInput  PSIn,
+//           out PSOutput PSOut)
+// {
+//     float4 Color = g_Texture.Sample(g_Texture_sampler, PSIn.UV);
+// #if CONVERT_PS_OUTPUT_TO_GAMMA
+//     // Use fast approximation for gamma correction.
+//     Color.rgb = pow(Color.rgb, float3(1.0 / 2.2, 1.0 / 2.2, 1.0 / 2.2));
+// #endif
+//     PSOut.Color = Color;
+// }
+//  )";
  
  
  class Tutorial00App
@@ -345,7 +404,7 @@ void main(in  PSInput  PSIn,
          return true;
      }
 
-	 void CreatePipelineState()
+	 void CreatePipelineState0()
 	 {
 		 // Pipeline state object encompasses configuration of all GPU stages
 
@@ -706,13 +765,126 @@ void main(in  PSInput  PSIn,
          close(fd);
 	 }
 
+	 void CreatePipelineState()
+	 {
+		 // Pipeline state object encompasses configuration of all GPU stages
+
+		 GraphicsPipelineStateCreateInfo PSOCreateInfo;
+
+		 // Pipeline state name is used by the engine to report issues.
+		 // It is always a good idea to give objects descriptive names.
+		 PSOCreateInfo.PSODesc.Name = "Cube PSO";
+
+		 // This is a graphics pipeline
+		 PSOCreateInfo.PSODesc.PipelineType = PIPELINE_TYPE_GRAPHICS;
+
+		 // clang-format off
+		 // This tutorial will render to a single render target
+		 PSOCreateInfo.GraphicsPipeline.NumRenderTargets = 1;
+		 // Set render target format which is the format of the swap chain's color buffer
+		 PSOCreateInfo.GraphicsPipeline.RTVFormats[0] = m_pSwapChain->GetDesc().ColorBufferFormat;
+		 // Set depth buffer format which is the format of the swap chain's back buffer
+		 PSOCreateInfo.GraphicsPipeline.DSVFormat = m_pSwapChain->GetDesc().DepthBufferFormat;
+		 // Primitive topology defines what kind of primitives will be rendered by this pipeline state
+		 PSOCreateInfo.GraphicsPipeline.PrimitiveTopology = PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+		 // Cull back faces
+		 PSOCreateInfo.GraphicsPipeline.RasterizerDesc.CullMode = CULL_MODE_BACK;
+		 // Enable depth testing
+		 PSOCreateInfo.GraphicsPipeline.DepthStencilDesc.DepthEnable = True;
+		 // clang-format on
+
+		 ShaderCreateInfo ShaderCI;
+		 // Tell the system that the shader source code is in HLSL.
+		 // For OpenGL, the engine will convert this into GLSL under the hood.
+		 ShaderCI.SourceLanguage = SHADER_SOURCE_LANGUAGE_HLSL;
+
+		 // OpenGL backend requires emulated combined HLSL texture samplers (g_Texture + g_Texture_sampler combination)
+		 ShaderCI.Desc.UseCombinedTextureSamplers = true;
+
+		 // Pack matrices in row-major order
+		 ShaderCI.CompileFlags = SHADER_COMPILE_FLAG_PACK_MATRIX_ROW_MAJOR;
+
+		 // Presentation engine always expects input in gamma space. Normally, pixel shader output is
+		 // converted from linear to gamma space by the GPU. However, some platforms (e.g. Android in GLES mode,
+		 // or Emscripten in WebGL mode) do not support gamma-correction. In this case the application
+		 // has to do the conversion manually.
+		 ShaderMacro Macros[] = { {"CONVERT_PS_OUTPUT_TO_GAMMA", m_ConvertPSOutputToGamma ? "1" : "0"} };
+		 ShaderCI.Macros = { Macros, _countof(Macros) };
+
+		 // In this tutorial, we will load shaders from file. To be able to do that,
+		 // we need to create a shader source stream factory
+// 		 RefCntAutoPtr<IShaderSourceInputStreamFactory> pShaderSourceFactory;
+// 		 m_pEngineFactory->CreateDefaultShaderSourceStreamFactory(nullptr, &pShaderSourceFactory);
+// 		 ShaderCI.pShaderSourceStreamFactory = pShaderSourceFactory;
+		 // Create a vertex shader
+		 RefCntAutoPtr<IShader> pVS;
+		 {
+			 ShaderCI.Desc.ShaderType = SHADER_TYPE_VERTEX;
+			 ShaderCI.EntryPoint = "main";
+			 ShaderCI.Desc.Name = "Cube VS";
+			 //ShaderCI.FilePath = "cube.vsh";
+			 ShaderCI.Source = VSSource;
+			 m_pDevice->CreateShader(ShaderCI, &pVS);
+			 // Create dynamic uniform buffer that will store our transformation matrix
+			 // Dynamic buffers can be frequently updated by the CPU
+			 BufferDesc CBDesc;
+			 CBDesc.Name = "VS constants CB";
+			 CBDesc.Size = sizeof(float4x4);
+			 CBDesc.Usage = USAGE_DYNAMIC;
+			 CBDesc.BindFlags = BIND_UNIFORM_BUFFER;
+			 CBDesc.CPUAccessFlags = CPU_ACCESS_WRITE;
+			 m_pDevice->CreateBuffer(CBDesc, nullptr, &m_VSConstants);
+		 }
+
+		 // Create a pixel shader
+		 RefCntAutoPtr<IShader> pPS;
+		 {
+			 ShaderCI.Desc.ShaderType = SHADER_TYPE_PIXEL;
+			 ShaderCI.EntryPoint = "main";
+			 ShaderCI.Desc.Name = "Cube PS";
+			 //ShaderCI.FilePath = "cube.psh";
+			 ShaderCI.Source = PSSource;
+			 m_pDevice->CreateShader(ShaderCI, &pPS);
+		 }
+
+		 // clang-format off
+		 // Define vertex shader input layout
+		 LayoutElement LayoutElems[] =
+		 {
+			 // Attribute 0 - vertex position
+			 LayoutElement{0, 0, 3, VT_FLOAT32, False},
+			 // Attribute 1 - vertex color
+			 LayoutElement{1, 0, 4, VT_FLOAT32, False}
+		 };
+		 // clang-format on
+		 PSOCreateInfo.GraphicsPipeline.InputLayout.LayoutElements = LayoutElems;
+		 PSOCreateInfo.GraphicsPipeline.InputLayout.NumElements = _countof(LayoutElems);
+
+		 PSOCreateInfo.pVS = pVS;
+		 PSOCreateInfo.pPS = pPS;
+
+		 // Define variable type that will be used by default
+		 PSOCreateInfo.PSODesc.ResourceLayout.DefaultVariableType = SHADER_RESOURCE_VARIABLE_TYPE_STATIC;
+
+		 m_pDevice->CreateGraphicsPipelineState(PSOCreateInfo, &m_pPSO);
+
+		 // Since we did not explicitly specify the type for 'Constants' variable, default
+		 // type (SHADER_RESOURCE_VARIABLE_TYPE_STATIC) will be used. Static variables never
+		 // change and are bound directly through the pipeline state object.
+		 m_pPSO->GetStaticVariableByName(SHADER_TYPE_VERTEX, "Constants")->Set(m_VSConstants);
+
+		 // Create a shader resource binding object and bind all static resources in it
+		 m_pPSO->CreateShaderResourceBinding(&m_SRB, true);
+	 }
+
      void CreateResources()
      {
          //InitFilament();
 		 CreatePipelineState();
-		 CreateVertexBuffer();
-		 CreateIndexBuffer();
-		 LoadTexture();
+		 //CreateVertexBuffer();
+		 //CreateIndexBuffer();
+		 InitFilament();
+		 //LoadTexture();
 		 return;
          // Pipeline state object encompasses configuration of all GPU stages
  
@@ -774,6 +946,55 @@ void main(in  PSInput  PSIn,
  
 	 void Render()
 	 {
+		 IDeviceContext* pCtx = m_pImmediateContext;// GetImmediateContext();
+		 pCtx->ClearStats();
+
+		 ITextureView* pRTV = m_pSwapChain->GetCurrentBackBufferRTV();
+		 ITextureView* pDSV = m_pSwapChain->GetDepthBufferDSV();
+		 pCtx->SetRenderTargets(1, &pRTV, pDSV, RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
+
+// 		 ITextureView* pRTV = m_pSwapChain->GetCurrentBackBufferRTV();
+// 		 ITextureView* pDSV = m_pSwapChain->GetDepthBufferDSV();
+		 // Clear the back buffer
+		 float4 ClearColor = { 0.350f, 0.350f, 0.350f, 1.0f };
+// 		 if (m_ConvertPSOutputToGamma)
+// 		 {
+// 			 // If manual gamma correction is required, we need to clear the render target with sRGB color
+// 			 ClearColor = LinearToSRGB(ClearColor);
+// 		 }
+		 m_pImmediateContext->ClearRenderTarget(pRTV, ClearColor.Data(), RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
+		 m_pImmediateContext->ClearDepthStencil(pDSV, CLEAR_DEPTH_FLAG, 1.f, 0, RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
+
+		 {
+			 // Map the buffer and write current world-view-projection matrix
+			 MapHelper<float4x4> CBConstants(m_pImmediateContext, m_VSConstants, MAP_WRITE, MAP_FLAG_DISCARD);
+			 *CBConstants = m_WorldViewProjMatrix;
+		 }
+
+		 // Bind vertex and index buffers
+		 const Uint64 offset = 0;
+		 IBuffer* pBuffs[] = { m_CubeVertexBuffer };
+		 m_pImmediateContext->SetVertexBuffers(0, 1, pBuffs, &offset, RESOURCE_STATE_TRANSITION_MODE_TRANSITION, SET_VERTEX_BUFFERS_FLAG_RESET);
+		 m_pImmediateContext->SetIndexBuffer(m_CubeIndexBuffer, 0, RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
+
+		 // Set the pipeline state
+		 m_pImmediateContext->SetPipelineState(m_pPSO);
+		 // Commit shader resources. RESOURCE_STATE_TRANSITION_MODE_TRANSITION mode
+		 // makes sure that resources are transitioned to required states.
+		 m_pImmediateContext->CommitShaderResources(m_SRB, RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
+
+		 DrawIndexedAttribs DrawAttrs;     // This is an indexed draw call
+		 DrawAttrs.IndexType = VT_UINT16;// VT_UINT32; // Index type
+		 DrawAttrs.NumIndices = 47232;// 36;
+		 // Verify the state of vertex and index buffers
+		 DrawAttrs.Flags = DRAW_FLAG_VERIFY_ALL;
+		 m_pImmediateContext->DrawIndexed(DrawAttrs);
+
+		 pCtx->SetRenderTargets(1, &pRTV, pDSV, RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
+	 }
+
+	 void Render0()
+	 {
 
 		 IDeviceContext* pCtx = m_pImmediateContext;// GetImmediateContext();
 		 pCtx->ClearStats();
@@ -813,8 +1034,8 @@ void main(in  PSInput  PSIn,
 		 m_pImmediateContext->CommitShaderResources(m_SRB, RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
 
 		 DrawIndexedAttribs DrawAttrs;     // This is an indexed draw call
-		 DrawAttrs.IndexType = VT_UINT32; // Index type
-		 DrawAttrs.NumIndices = 36;
+		 DrawAttrs.IndexType = VT_UINT16;// VT_UINT32; // Index type
+		 DrawAttrs.NumIndices = 47232;// 36;
 		 // Verify the state of vertex and index buffers
 		 DrawAttrs.Flags = DRAW_FLAG_VERIFY_ALL;
 		 m_pImmediateContext->DrawIndexed(DrawAttrs);
@@ -901,7 +1122,7 @@ void main(in  PSInput  PSIn,
 		 m_WorldViewProjMatrix = CubeModelTransform * View * SrfPreTransform * Proj;
 	 }
 
-     void Render0()
+     void Render_()
      {
          // Set render targets before issuing any draw command.
          // Note that Present() unbinds the back buffer if it is set as render target.
